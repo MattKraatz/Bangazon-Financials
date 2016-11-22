683e7a85bf8b930a68adf679f3e9bbeab6383b3b
Matt Kraatz <Matt.Kraatz@gmail.com>
2016-11-22 15:31:39 -0600

    completed menu system

---
+++ src/Bangazon-Financials/Actions/ReportActions.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bangazon_Financials
+{
+    public class ReportActions
+    {
+        public static void WeeklyReport()
+        {
+            Console.WriteLine(@"
+==========================
+BANGAZON FINANCIAL REPORTS
+==========================
+
+WEEKLY REPORT
+
+Product                       Amount
+-------------------------------------
+");
+            var conn = new DatabaseConnection();
+            List<Revenue> output = conn.getWeek();
+            output.ForEach(delegate (Revenue line)
+            {
+                StringBuilder buffer = new StringBuilder();
+                for (int i = 0; i < 30 - line.ProductName.Count(); i++)
+                {
+                    buffer.Append(" ");
+                }
+                Console.WriteLine($"{line.ProductName}{buffer}{line.ProductRevenue}");
+            });
+            Console.WriteLine("Press Enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void MonthlyReport()
+        {
+            Console.WriteLine(@"
+==========================
+BANGAZON FINANCIAL REPORTS
+==========================
+
+MONTHLY REPORT
+
+Product                       Amount
+-------------------------------------
+");
+            var conn = new DatabaseConnection();
+            List<Revenue> output = conn.getMonth();
+            output.ForEach(delegate (Revenue line)
+            {
+                StringBuilder buffer = new StringBuilder();
+                for (int i = 0; i < 30 - line.ProductName.Count(); i++)
+                {
+                    buffer.Append(" ");
+                }
+                Console.WriteLine($"{line.ProductName}{buffer}{line.ProductRevenue}");
+            });
+            Console.WriteLine("Press Enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void QuarterlyReport()
+        {
+            Console.WriteLine(@"
+==========================
+BANGAZON FINANCIAL REPORTS
+==========================
+
+QUARTERLY REPORT
+
+Product                       Amount
+-------------------------------------
+");
+            var conn = new DatabaseConnection();
+            List<Revenue> output = conn.getQuarter();
+            output.ForEach(delegate (Revenue line)
+            {
+                StringBuilder buffer = new StringBuilder();
+                for (int i = 0; i < 30 - line.ProductName.Count(); i++)
+                {
+                    buffer.Append(" ");
+                }
+                Console.WriteLine($"{line.ProductName}{buffer}{line.ProductRevenue}");
+            });
+            Console.WriteLine("Press Enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void CustomerReport()
+        {
+            Console.WriteLine(@"
+==========================
+BANGAZON FINANCIAL REPORTS
+==========================
+
+CUSTOMER REVENUE REPORT
+
+Customer                      Amount
+-------------------------------------
+");
+            var conn = new DatabaseConnection();
+            List<Revenue> output = conn.getByCustomer();
+            output.ForEach(delegate (Revenue line)
+            {
+                StringBuilder buffer = new StringBuilder();
+                for (int i = 0; i < 29 - (line.CustomerFirstName.Count() + line.CustomerLastName.Count()); i++)
+                {
+                    buffer.Append(" ");
+                }
+                Console.WriteLine($"{line.CustomerFirstName} {line.CustomerLastName}{buffer}{line.ProductRevenue}");
+            });
+            Console.WriteLine("Press Enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void ProductReport()
+        {
+            Console.WriteLine(@"
+==========================
+BANGAZON FINANCIAL REPORTS
+==========================
+
+PRODUCT REVENUE REPORT
+
+Product                       Amount
+-------------------------------------
+");
+            var conn = new DatabaseConnection();
+            List<Revenue> output = conn.getByProduct();
+            output.ForEach(delegate (Revenue line)
+            {
+                StringBuilder buffer = new StringBuilder();
+                for (int i = 0; i < 30 - (line.ProductName.Count()); i++)
+                {
+                    buffer.Append(" ");
+                }
+                Console.WriteLine($"{line.ProductName}{buffer}{line.ProductRevenue}");
+            });
+            Console.WriteLine("Press Enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+    }
+}
--- src/Bangazon-Financials/Program.cs
+++ src/Bangazon-Financials/Program.cs
@@ -9,6 +9,69 @@ namespace Bangazon_Financials
     {
         public static void Main(string[] args)
         {
+            var AcceptableInputs = new int[5] { 1, 2, 3, 4, 5 };
+            int selection = 0;
+            while (!AcceptableInputs.Contains(selection))
+            {
+                Console.WriteLine(@"
+==========================
+BANGAZON FINANCIAL REPORTS
+==========================
+
+1. Weekly Report
+2. Monthly Report
+3. Quarterly Report
+4. Customer Revenue Report
+5. Product Revenue Report
+X. Exit Program");
+
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "x")
+                {
+                    break;
+                }
+
+                try
+                {
+                    selection = Convert.ToInt32(input);
+                }
+                catch
+                {
+                    Console.WriteLine("I don't recognize that selection, please try again.");
+                    selection = 0;
+                    continue;
+                }
+
+                switch(selection)
+                {
+                    case 1:
+                        ReportActions.WeeklyReport();
+                        selection = 0;
+                        break;
+                    case 2:
+                        ReportActions.MonthlyReport();
+                        selection = 0;
+                        break;
+                    case 3:
+                        ReportActions.QuarterlyReport();
+                        selection = 0;
+                        break;
+                    case 4:
+                        ReportActions.CustomerReport();
+                        selection = 0;
+                        break;
+                    case 5:
+                        ReportActions.ProductReport();
+                        selection = 0;
+                        break;
+                    default:
+                        Console.WriteLine("I don't recognize that selection, please try again.");
+                        selection = 0;
+                        break;
+                }
+
+            }
         }
     }
 }
