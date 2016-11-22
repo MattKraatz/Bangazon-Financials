a52451f6834641f1d0054002db52c3538a716e52
Matt Kraatz <Matt.Kraatz@gmail.com>
2016-11-22 13:53:00 -0600

    completed monthly report database query

--- src/Bangazon-Financials/Data/DatabaseConnection.cs
+++ src/Bangazon-Financials/Data/DatabaseConnection.cs
@@ -55,6 +55,46 @@ namespace Bangazon_Financials
             return output;
         }
 
+        public List<Revenue> getMonth()
+        {
+            var output = new List<Revenue>();
+
+            execute(@"select Id,
+                        ProductName,
+                        ProductCost,
+                        ProductRevenue,
+                        ProductSupplierState,
+                        CustomerFirstName,
+                        CustomerLastName,
+                        CustomerAddress,
+                        CustomerZipCode,
+                        PurchaseDate
+                    from Revenue
+                    where PurchaseDate BETWEEN DateTime('now','-1 month') AND DateTime('now')",
+                    (SqliteDataReader reader) =>
+                    {
+                        while (reader.Read())
+                        {
+                            output.Add(new Revenue
+                            {
+                                Id = reader.GetInt32(0),
+                                ProductName = reader[1].ToString(),
+                                ProductCost = reader.GetInt32(2),
+                                ProductRevenue = reader.GetInt32(3),
+                                ProductSupplierState = reader[4].ToString(),
+                                CustomerFirstName = reader[5].ToString(),
+                                CustomerLastName = reader[6].ToString(),
+                                CustomerAddress = reader[7].ToString(),
+                                CustomerZipCode = reader.GetInt32(8),
+                                PurchaseDate = Convert.ToDateTime(reader[9].ToString())
+                            });
+                        }
+                    });
+
+            return output;
+        }
+
+
         /**
          * Purpose: Execute SQL statement
          * Arguments:
