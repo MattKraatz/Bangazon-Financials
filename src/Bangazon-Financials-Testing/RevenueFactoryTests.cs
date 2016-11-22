6ff63de95d1a7cc64556c8d4725a20be8131fbe8
Matt Kraatz <Matt.Kraatz@gmail.com>
2016-11-22 13:46:14 -0600

    added test for monthly report

--- src/Bangazon-Financials-Testing/RevenueFactoryTests.cs
+++ src/Bangazon-Financials-Testing/RevenueFactoryTests.cs
@@ -33,5 +33,29 @@ namespace Bangazon_Financials_Testing
             });
         }
 
+        [Fact]
+        public void CanGetRevenueFromPastMonth()
+        {
+            var conn = new DatabaseConnection();
+            List<Revenue> rev = conn.getMonth();
+            Assert.NotNull(rev);
+            Assert.True(rev.Count > 1);
+            rev.ForEach(delegate (Revenue item)
+            {
+                Assert.NotNull(item.CustomerAddress);
+                Assert.NotNull(item.CustomerFirstName);
+                Assert.NotNull(item.CustomerLastName);
+                Assert.NotNull(item.CustomerZipCode);
+                Assert.NotNull(item.Id);
+                Assert.NotNull(item.ProductCost);
+                Assert.NotNull(item.ProductName);
+                Assert.NotNull(item.ProductRevenue);
+                Assert.NotNull(item.ProductSupplierState);
+                Assert.NotNull(item.PurchaseDate);
+                Assert.IsType<DateTime>(item.PurchaseDate);
+                Assert.True(item.PurchaseDate > DateTime.Today.AddMonths(-1));
+            });
+        }
+
     }
 }
