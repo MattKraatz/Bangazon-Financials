66616fb12b566b948b91a4b48c2269bae3666e21
Matt Kraatz <Matt.Kraatz@gmail.com>
2016-11-22 11:33:56 -0600

    boilerplate project and testing setup

---
+++ src/Bangazon-Financials-Testing/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bangazon_Financials;
+using Xunit;
+
+
+namespace Bangazon_Financials_Testing
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestingFrameworkLoads()
+        {
+            Assert.True(true);
+        }
+    }
+}
